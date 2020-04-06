f8d38e6f5eb67b90ee72944102715022327637b3
alexao8 <61477091+alexao8@users.noreply.github.com>
2020-04-06 21:55:47 +0200

    Update envio.php

--- envio.php
+++ envio.php
@@ -1,15 +1,12 @@
-
 <?php 
-$myemail = 'alex.menorca@gmail.com';
+$mail = 'alex.menorca@gmail.com';
 $name = $_POST['nombre'];
 $email = $_POST['email'];
 $subject = $_POST['asunto'];
 $message = $_POST['msg'];
 
-$email_subject = "Nuevo mensaje: $subject";
-$email_body = "Has recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
-$headers = "From: $email";
+$header = "From: $email";
 
-mail($myemail, $email_subject, $email_body, $headers);
+mail($mail, $subject, $message, $header);
 echo "El mensaje se ha enviado correctamente";
 ?>
