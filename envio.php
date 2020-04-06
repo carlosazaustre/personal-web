567c1ba7eb82a20039819ee6586aed7bd3385962
alexao8 <61477091+alexao8@users.noreply.github.com>
2020-04-06 21:35:16 +0200

    Add files via upload

---
+++ envio.php
@@ -0,0 +1,16 @@
+
+<?php 
+$myemail = 'alex.menorca@gmail.com';
+$name = $_POST['nombre'];
+$email = $_POST['email'];
+$issue = $_POST['asunto'];
+$message = $_POST['msg'];
+
+$email_subject = "Nuevo mensaje: $subject";
+$email_body = "Has recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+$headers = "From: $email";
+
+mail($myemail, $email_subject, $email_body, $headers);
+echo "El mensaje se ha enviado correctamente";
+echo "<script> setTimeout(\'location-href=index.html'\',1000)</script>;
+?>
