692b213ff87d371a049bea36f8a7875449820829
Carlos Azaustre <cazaustre@gmail.com>
2020-04-11 11:28:20 +0200

    enable firebase database and create a clousure

--- envio.js
+++ envio.js
@@ -1,3 +1,4 @@
+(function () {
   var firebaseConfig = {
     apiKey: "AIzaSyDe6jloKy_0iykTw7HtGyKM62ZmJ5Npfo0",
     authDomain: "personal-web-b95bd.firebaseapp.com",
@@ -6,40 +7,38 @@
     storageBucket: "personal-web-b95bd.appspot.com",
     messagingSenderId: "611454640863",
     appId: "1:611454640863:web:4cc815ae45ad1f05443f6a",
-    measurementId: "G-DZ22M9BVPL"
+    measurementId: "G-DZ22M9BVPL",
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-   
-  var messagesRef = firebase.database().ref('messages');
 
-document.getElementById('cont').addEventListener('submit', submitForm);
+  var messagesRef = firebase.database().ref("messages");
 
-function submitForm(e) {
+  document.getElementById("cont").addEventListener("submit", submitForm);
+
+  function submitForm(e) {
     e.preventDefault();
-    
-    var name = getInputVal('nombre');
-    var email = getInputVal('email');
-    var asunto = getInputVal('asunto');
-    var msg = getInputVal('msg');
-    
-    saveMessage(name,email,asunto,msg);
-    
-    
-}
 
-function getInputVal(id) {
+    var name = getInputVal("nombre");
+    var email = getInputVal("email");
+    var asunto = getInputVal("asunto");
+    var msg = getInputVal("msg");
+
+    saveMessage(name, email, asunto, msg);
+  }
+
+  function getInputVal(id) {
     return document.getElementById(id).value;
-}
+  }
 
-function saveMessage(name,email,asunto,msg) {
+  function saveMessage(name, email, asunto, msg) {
     var newMessageRef = messagesRef.push();
-    newMessageRef.set( {
-        name:name,
-        email:email,
-        asunto:asunto,
-        msg:msg
-        
+    newMessageRef.set({
+      name: name,
+      email: email,
+      asunto: asunto,
+      msg: msg,
     });
-}
+  }
+})();
